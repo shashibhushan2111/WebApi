390a4182741cd08989d03fc5a45e6ea883f3c7d5
DELL <DELL@DESKTOP-KREPKH7>
2022-06-26 13:49:11 +0530

    Add project files.

---
+++ WebApi/Controllers/SuperHeroController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        public static List<SuperHero> heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    ID = 1,
+                    Name = "father",
+                    Power = "PowerLike_GOD"
+                }
+            };
+        private readonly ApplicationDbContext _context;
+
+        public SuperHeroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+
+            return Ok(await _context.superHeroes.ToListAsync());
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var hero = await _context.superHeroes.FindAsync(id);
+            return Ok(hero);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddHero(SuperHero hero)
+        {
+            _context.superHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.superHeroes.ToListAsync());
+        }
+        [HttpPut]
+        public async Task<IActionResult> PutHero(SuperHero Req)
+        {
+            var hero = await _context.superHeroes.FindAsync(Req.ID);
+            hero.Name = Req.Name;
+            hero.Power = Req.Power;
+            await _context.SaveChangesAsync();
+            return Ok(await _context.superHeroes.ToListAsync());
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delet(int id)
+        {
+            var hero = await _context.superHeroes.FindAsync(id);
+            _context.superHeroes.Remove(hero);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.superHeroes.ToListAsync());
+        }
+    }
+}
---
+++ WebApi/SuperHero.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi
+{
+    public class SuperHero
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Power { get; set; }
+
+    }
+}
